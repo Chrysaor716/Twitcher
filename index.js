18297b7de620b0009f8aecb486b4da67716afef3
Christina Nguyen <phuonghien716@gmail.com>
2016-01-24 21:25:00 -0500

    Twitter keys and tokens added.

--- index.js
+++ index.js
@@ -1,36 +1,24 @@
 var express = require('express'),
     http = require('http'),
     path = require('path');
-
-//var TwitchClient = require("node-twitchtv");
-//
-//	Integrate Twitch
-//var account = require("../secrets/user.json");
-//var client = new TwitchClient(account);
-//
-//client.games({limit: 20, offset: 21}, function(err, response) {
-//	console.log(games);
-//});
-//
-////	Retrieve a user's information
-//client.users({user: "TorchedMuffinz"}, function(err, response) {
-//	console.log(user.name); // User information
-//});
-//
-////	Retrieve a channel's information
-//client.channels({channel: "Torchedmuffinz"}, function(err, response) {
-//	console.log(channel.info); // Channel info/description
-//});
-//
-////	Retrieve a stream's information
-//client.streams({channel: "Torchedmuffinz"}, function(err, response) {
-//
-//});
+var Twitter = require('twitter-js-client').Twitter;
 
 var app = express(); // Create an instance of express
 
+/*	Callbacks	*/
+var error = function(err, response, body) {
+	console.log('ERROR [%s]', err);
+};
+var success = function(data) {
+	console.log('Data [%s]', data);
+};
+
+var config = {
+	"callBackUrl": ""
+}
+
 app.get('/', function(req, res) {
-	res.send('GET request to the homepage');
+	res.send('\'SUP, HOMIES');
 });
 
 app.listen(3000, function() {
